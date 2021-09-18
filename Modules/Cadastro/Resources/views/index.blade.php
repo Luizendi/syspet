7170eb26bdb11a5d52709abd36890a886a16caa8
Renan Rodrigues de Oliveira <43794964+renanthole@users.noreply.github.com>
2021-09-18 15:40:29 -0300

    v1.0

---
+++ Modules/Cadastro/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('cadastro::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('cadastro.name') !!}
+    </p>
+@endsection
